ccd3872aad85ea3abcaef5dbf0f4ff080717407e
kyouuko <gcmarino99@gmail.com>
2025-02-16 22:30:13 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      im the app
+    </>
+  )
+}
+
+export default App
